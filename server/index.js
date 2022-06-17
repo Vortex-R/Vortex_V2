43423b71aa0015d0393f3374b9512ef96f103cc3
malek-haddar <malek.haddar1@esprit.tn>
2022-06-17 04:24:11 +0100

    set to prod

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use("/api/organizerP", auth, organizerPRoute);
 app.use("/api/userP", auth, userPRoute);
 
 // Serve Old frontend
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(process.cwd(), "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(process.cwd(), "client/build/index.html"));
