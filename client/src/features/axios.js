43705450f3474f1a28116d2e232e73849411d388
malek-haddar <malek.haddar1@esprit.tn>
2022-09-10 16:36:47 +0100

    change cyclic url 2

--- client/src/features/axios.js
+++ client/src/features/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "https://scouts-tunisienne.herokuapp.com/",
   // baseURL: "http://localhost:5000/",
-  baseURL: "https://fine-ruby-bunny-shoe.cyclic.app/",
+  baseURL: "https://vortexreaction.cyclic.app/",
 });
 
 export default instance;
