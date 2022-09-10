eb341f097d1465e45c63619983c7d0411d772596
malek-haddar <malek.haddar1@esprit.tn>
2022-09-10 16:43:31 +0100

    fix url 3

--- client/src/features/axios.js
+++ client/src/features/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "https://scouts-tunisienne.herokuapp.com/",
   // baseURL: "http://localhost:5000/",
-  baseURL: "https://vortexreaction.cyclic.app/",
+  // baseURL: "https://vortexreaction.cyclic.app/",
+  baseURL: "https://fine-ruby-bunny-shoe.cyclic.app/",
 });
 
 export default instance;
